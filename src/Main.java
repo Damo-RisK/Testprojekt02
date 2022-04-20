165d37800e97f5497b340564aa7e66a985bbad77
Damo-RisK <damian.siol@googlemail.com>
2022-04-20 10:21:08 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Das ist ein Test der IDE!");
+        System.out.println("Oh nein eine Änderung!");
+    }
+}
